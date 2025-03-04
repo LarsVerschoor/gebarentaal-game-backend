30bdb42ba8b770ad234b32fa332e068b00da3301
LarsVerschoor <larsverschoor2@gmail.com>
2025-03-04 10:40:41 +0100

    Added user and session

---
+++ database/migrations/20250304093504-create-session.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Sessions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.BIGINT
+      },
+      token: {
+        type: Sequelize.STRING
+      },
+      expiresAt: {
+        type: Sequelize.DATE
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Sessions');
+  }
+};
