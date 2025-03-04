443ee2259cffcdf1a2e7d18676b751a45e2b4245
LarsVerschoor <larsverschoor2@gmail.com>
2025-03-04 13:36:36 +0100

    dropped sessions table

---
+++ database/migrations/20250304122955-drop-sessions-table.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Sessions');
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.createTable('Sessions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT
+      },
+      userId: {
+        type: Sequelize.BIGINT
+      },
+      token: {
+        type: Sequelize.STRING
+      },
+      expiresAt: {
+        type: Sequelize.DATE
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  }
+};
