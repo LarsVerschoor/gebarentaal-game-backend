8243cad65a994c3ed61ac27db39e4c0583557f5b
Geert van der Velden <1079252@hr.nl>
2025-03-10 13:05:24 +0100

    routing base

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import express from 'express';
-import gameRouter from './routes/v1/gameRouter';
 const express = require('express')
-const {Lesson, Word} = require('./database/models');
+// const {Lesson, Word} = require('./database/models');
 const app = express();
+const v1GameRouter = require('./routes/v1/v1GameRouter')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
@@ -22,8 +21,18 @@ app.use((req, res, next) => {
     next()
 })
 
+app.use((req, res, next) => {
+    const acceptHeader = req.headers['accept'];
+    if (acceptHeader.includes('application/json') || req.method === 'OPTIONS' ){
+        next()
+    } else {
+        res.status(406).send('Illegal format, only use json');
+    }
+
+})
+
 // Hier zouden dus meerdere versies van de API geregistreerd kunnen worden
-app.use('/api/v1/', gameRouter);
+app.use('/api/v1/', v1GameRouter);
 
 const port = process.env.EXPRESS_PORT || 8000;
 app.listen(port , () => {
--- routes/v1/gameRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const gameRouter = new Router();
-
-gameRouter.get('/words/:id', async (req, res) =>{
-    const id = req.params.id;
-    // const word = await
-})
-export default gameRouter;
