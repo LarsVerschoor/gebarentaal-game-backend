74b57fc01dc2f47731dcd4e2d35e1e3e5b88c278
LarsVerschoor <larsverschoor2@gmail.com>
2025-03-12 14:36:15 +0100

    test2

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ app.use(express.urlencoded({extended: true}));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     next();
 });
 
