f61af65932d016ee780c5742752c2b32ef2ac902
LarsVerschoor <larsverschoor2@gmail.com>
2025-03-12 14:15:40 +0100

    test

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     next();
 });
 
